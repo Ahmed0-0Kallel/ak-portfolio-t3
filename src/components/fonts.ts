b5d1c3d941aedf52fbde87a93ce24889f784a086
Ahmed0-0Kallel <contactahmedkallel0@gmail.com>
2023-05-05 23:33:31 +0200

    Added userData and Fonts | Added NavBar

---
+++ src/components/fonts.ts
@@ -0,0 +1,7 @@
+import { NextFont } from "@next/font";
+import { Inter, Roboto_Mono } from "@next/font/google";
+
+export const inter300 : NextFont = Inter({ weight: "300", subsets: ["latin"] });
+export const inter700 : NextFont = Inter({ weight: "700", subsets: ["latin"] });
+export const inter800 : NextFont = Inter({ weight: "800", subsets: ["latin"] });
+export const RobotoMono300 : NextFont = Roboto_Mono({ weight: "300", subsets: ["latin"] });
---
+++ src/constatnts/data.ts
@@ -0,0 +1,42 @@
+const userData = {
+    githubUsername: "ahmed0-0kallel",
+    name: "Ahmed Kallel",
+    designation: "Software Engineer",
+    avatarUrl: "/avatar.jpg",
+    email: "contactahmedkallel0@gmail.com",
+    location: "Budapest, HU",
+    projectDescription: "My portfolio Website",
+    projects: [
+      // {
+      //   title: "Illawarra",
+      //   link: "https://illawarratherapies.com.au",
+      //   imgUrl: "/illawarra.jpg",
+      // },
+    ],
+    about: {
+      title:
+        "Full stack developer with 7+ years of experience building scalable web applications and internal tools that drastically improve efficiency.",
+      description: [
+        "I have worked on a large range of projects and developed strong skills in software engineering by delivering applications for native platforms and web. In the past decade I worked with entrepreneurs and small to medium sized businesses.",
+        "I am Full-Stack Developer — my front-end and back-end expertise come together, essentially covering the full stack of technology for a project. Well versed with Front-End Tools like HTML, CSS, JavaScript; Back-End technologies and infrastructure: NodeJS, Express, NestJS, Fastify.",
+      ]
+    },
+    experience: [
+      {
+        title: "Supervisory Controls and Data Acquisition Engineer",
+        company: "Tesco Controls",
+        year: "2021",
+        companyLink: "https://tescocontrols.com",
+        description: "Functioned as the technical lead on projects of medium to large scale. Provided mentoring and training of junior staff. Designed complex systems for operating manufacturing processes and water/wastewater facilities, collecting data in real-time and alarming based on certain conditions",
+      }
+
+
+    ],
+    socialLinks: {
+      linkedin: "https://linkedin.com/in/dstolbov",
+      github: "https://github.com/goosewin",
+    },
+  };
+  
+  export default userData;
+  
